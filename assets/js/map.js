91f91b3b4de80d3679580b91219b21eba04a67e2
Tzu-ping Chung <uranusjr@gmail.com>
2014-09-27 00:09:11 +0800

    Fallback to Places API if Geocoding fails

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,25 +1,28 @@
 function initialize() {
-  geocoder  = new google.maps.Geocoder();
 
-  var defaultLatLng    = new google.maps.LatLng(-34.397, 150.644),
-      address          = document.getElementById('location-content').innerHTML,
-      mapOptions = {
-        zoom: 14,
-        center: defaultLatLng
-      };
+  var updateMap = function (map, center) {
+    map.setCenter(center);
+    var marker = new google.maps.Marker({'map': map, 'position': center});
+  };
 
+  var map = new google.maps.Map(document.getElementById('map-canvas'), {
+    'zoom': 14, 'center': new google.maps.LatLng(25.03518, 121.54388)   // CLBC.
+  });
+
+  var address = document.getElementById('location-content').innerHTML;
+
+  var geocoder = new google.maps.Geocoder();
   geocoder.geocode({'address': address}, function(results, status) {
-    if (status == google.maps.GeocoderStatus.OK) {
-      mapOptions.center=results[0].geometry.location;
-    } else {
-      // alert('Geocode was not successful for the following reason: ' + status);
+    if (status == google.maps.GeocoderStatus.OK && results.length) {
+      updateMap(map, results[0].geometry.location);
+    } else if (google.maps.places) {
+      var service = new google.maps.places.PlacesService(map);
+      var request = {'location': map.getCenter(), 'radius': 50000, 'query': address};
+      service.textSearch(request, function (results, status) {
+        if (status == google.maps.GeocoderStatus.OK && results.length)
+          updateMap(map, results[0].geometry.location);
+      });
     }
-
-    map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
-    var marker = new google.maps.Marker({
-        map: map,
-        position: mapOptions.center
-    });
   });
 }
 
